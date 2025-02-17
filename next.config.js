52de02bfa37aee38b3b56ddaff86e2cc9e32acc9
Zlatko <zmatokan@gmail.com>
2025-02-17 08:29:19 +0100

    Configure for AWS Amplify deployment

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
+  output: 'export',
 }
 
 module.exports = nextConfig 
